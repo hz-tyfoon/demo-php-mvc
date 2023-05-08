6c03d920427b76067fa6e766bf165965c27eebbb
hz-tyfoon <md.tifun@gmail.com>
2023-05-08 18:34:22 +0600

    prettified create new post form with tilwind

--- views/partials/header.php
+++ views/partials/header.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stacked UI</title>
-    <script src="../../assets/scripts/tailwind.main.js"></script>
+    <!-- <script src="../../assets/scripts/tailwind.main.js"></script> -->
+    <script src="../../assets/scripts/tailwind-with-forms.js"></script>
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
 
--- views/posts-create.view.php
+++ views/posts-create.view.php
@@ -3,13 +3,22 @@
 <?php require 'partials/banner.php' ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <form method="post">
-            <div>
-                <label for="postBody">
-                    <textarea name="postBody" cols="30" rows="10" placeholder="your post name"></textarea>
-                </label>
+        <form>
+            <div class="border-b border-gray-900/10 pb-3">
+                <div class="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="col-span-full">
+                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Post Title</label>
+                        <div class="mt-2">
+                            <textarea id="about" name="title" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-            <button type="submit">submit</button>
+
+            <div class="mt-6 flex items-center justify-start gap-x-6">
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">submit</button>
+            </div>
         </form>
     </div>
 </main>
