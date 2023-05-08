6eab0169e43dcbf22d4849bf8b3bd724b4f8be34
hz-tyfoon <md.tifun@gmail.com>
2023-05-08 12:25:38 +0600

    refactor: moved database works & config to separate files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database'  => [
+        'host'      => '127.0.0.1',
+        'port'      => '3306',
+        'dbname'    => 'demo-mvc',
+        'charset'   => 'utf8mb4',
+    ],
+    // some other config can go here
+];
--- index.php
+++ index.php
@@ -2,4 +2,17 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+require "Database.php";
+
+$config = require "./config.php";
+
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts")->fetchAll();
+$posts_greater_than_1 = $db->query("select * from posts")->fetch();
+
+dd(
+    $posts,
+    // $posts_greater_than_1
+);
