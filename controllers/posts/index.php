0fdc2bf3e71b5911913b3d6100117909789829f8
hz-tyfoon <md.tifun@gmail.com>
2023-05-10 17:30:30 +0600

    ignore: set user_id to 3 to match across the app

--- controllers/posts/index.php
+++ controllers/posts/index.php
@@ -4,7 +4,7 @@ use Core\Database;
 
 $config = require basePath("config.php");
 
-$user_id = 5;
+$user_id = 3;
 
 $db = new Database($config['database']);
 $posts = $db->query("select * from posts where user_id = :user_id", ['user_id' => $user_id])->getAll();
