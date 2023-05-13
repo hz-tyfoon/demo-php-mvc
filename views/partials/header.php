2ffd8cf317b1d63bd66b0ab8d68401db29b8ae14
hz-tyfoon <md.tifun@gmail.com>
2023-05-13 15:31:45 +0600

    refactor: moved css in a separate scss file and linked the compiled css file

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,60 +4,8 @@
 
 <head>
   <title>To do list App</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-    }
-
-    h1.top-head {
-      text-align: center;
-      font-size: 30px;
-      font-weight: 800;
-    }
-
-    .task-list {
-      list-style-type: none;
-      padding: 0;
-    }
-
-    .task-item {
-      border: 1px solid #ccc;
-    }
-
-    .task-item .title {
-      font-weight: bold;
-    }
-
-    .task-item .expired {
-      color: red;
-    }
-
-    .task-item .deadline {
-      font-size: 0.9em;
-    }
-
-    .task-item .actions {
-      margin-top: 5px;
-    }
-
-    .add-task-form input[type="text"],
-    .add-task-form textarea,
-    .add-task-form input[type="date"] {
-      width: 700px;
-    }
-    .delete-button {
-      background-color: #ff0000;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-    }
-
-    .checkmark {
-      margin-right: 5px;
-    }
-  </style>
-
   <!-- <script src="https://cdn.tailwindcss.com?plugins=forms"></script> -->
+  <link rel="stylesheet" href="<?php echo $base_uri ?>assets/css/style.css">
   <script src="<?php echo $base_uri ?>assets/scripts/tailwind-with-forms.js"></script>
 
 </head>
