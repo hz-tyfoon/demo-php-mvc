9c1812b8b9d291550457533fe3cd2d18282c7227
hz-tyfoon <md.tifun@gmail.com>
2023-05-13 20:23:11 +0600

    WIP: deleting item: sent ajax request to 'todos/{id}' endpoint to delete the item

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,5 +1,13 @@
 const hanldeDeleteTaksItem = (id) => {
-  console.log("hanldeDeleteTaksItem triggered");
+  const xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "/todos/" + id, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      console.log("Item deleted with ID:", id);
+    }
+  };
+  xhr.send();
 };
 const confirmDeleteTaksItem = (id) => {
   if (confirm("Are you sure you want to delete this item?")) {
